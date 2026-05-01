a35e34ced63b72baffe39016827977da28285bf8
kompotkot <zenitsws@gmail.com>
2026-05-01 11:59:53 +0000

    Indexes and updated trigger

--- db/migrations/011-invites.sql
+++ db/migrations/011-invites.sql
@@ -5,12 +5,21 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS invites (
     id VARCHAR(255) NOT NULL, 
     user_id UUID, 
+    
     description VARCHAR, 
+    
     created_at TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL, 
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL, 
+
     CONSTRAINT pk_invites PRIMARY KEY (id), 
     CONSTRAINT fk_invites_user_id_users FOREIGN KEY(user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+DROP TRIGGER IF EXISTS trg_invites_set_updated_at ON invites;
+CREATE TRIGGER trg_invites_set_updated_at
+BEFORE UPDATE ON invites
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
 
 UPDATE version_auth SET version_num = 'fd168c58bc02' RETURNING version_auth.version_num;
 
